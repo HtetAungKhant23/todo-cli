69dcf6845eb898c79d80068022fe7e5fce0eceb8
Htet Aung Khant <htetaung23.ha@gmail.com>
2025-09-29 00:04:57 +0630

    feat(todo): implement json file as database for todos

--- main.go
+++ main.go
@@ -8,10 +8,13 @@ import (
 func main() {
 	fmt.Println("Hello from TODO CLI!")
 	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+	storage.load(&todos)
 
 	deadline := time.Now().AddDate(0, 0, 2)
 	todos.add("Learn how http work deeply.", &deadline)
 	todos.add("Learn GoLang.", nil)
+	storage.save(todos)
 
 	todos.list()
 }
